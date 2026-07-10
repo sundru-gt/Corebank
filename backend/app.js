a90ea060857811d41407b9cc8685209670382d3b
sundru <shivendrupaul@gmail.com>
2026-07-10 13:49:00 +0530

    implemented account model with authentication middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const cookieParser=require('cookie-parser')
 const authRouter=require("./routes/auth.routes")
+const accountRouter=require("./routes/account.routes")
 const app= express();
 
 //to use the req.body
@@ -8,4 +9,5 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRouter)
+app.use("/api/accounts",accountRouter)
 module.exports=app;
